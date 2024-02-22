09c0890c48d22c2cc6874676eaad47c3cbc6bfa1
sboyd5 <157087132+sboyd5@users.noreply.github.com>
2024-02-22 19:32:30 +0000

    updated project sprite

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /**
  * Project 1 - Interactive Image
- * Name: 
- * Comments: 
+ * Name: Savannah Boyd
+ * Comments: A spooky spider!
  */
 
 // Global Variables go here
@@ -17,10 +17,15 @@ function draw(){
   background(100); //light gray background
   
   // add your image drawing code here 
-  rect(100, 150, 210, 20); //horizontal to spider leg one
-  rect(100, 150, 15, 60); // spider leg one
-  ellipse(200, 150, 110, 90); //spider body base
- 
+  rect(120, 160, 195, 15); // bottom horizontal to spider leg one
+  rect(120, 160, 15, 45); // bottom spider leg one
+  rect(300, 160, 15, 45); //  bottom leg piece two 
+  rect(120, 130, 195, 15); // middle leg
+  rect(120, 100, 195, 15); // top leg base
+  rect(120, 55, 15, 45); // left top leg
+  rect(300, 55, 15, 45); // right top leg
+  ellipse(215, 150, 115, 140); //spider body base 
+  fill(0);
 /* 
   Use the following if()...else() structure to incorporate mouse click control of your animation
 */
